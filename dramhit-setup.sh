bba1eb2859f92dc1e5819b2617364d310e8fa355
Joshua Tlatelpa-Agustin <joshua.a.tlatelpa@gmail.com>
2025-08-09 21:52:53 -0600

    fixed nvme setup

--- dramhit-setup.sh
+++ dramhit-setup.sh
@@ -55,13 +55,13 @@ install_dependencies() {
 }
 
 create_extfs() {
-  record_log "Creating ext4 filesystem on /dev/nvme0n1p4"
-  sudo mkfs.ext4 -Fq /dev/nvme0n1p4
+  record_log "Creating ext4 filesystem on /dev/nvme1n1"
+  sudo mkfs.ext4 -Fq /dev/nvme1n1
 }
 
 mountfs() {
   sudo mkdir -p ${MOUNT_DIR}
-  sudo mount -t ext4 /dev/nvme0n1p4 ${MOUNT_DIR}
+  sudo mount -t ext4 /dev/nvme1n1 ${MOUNT_DIR}
 
   if [[ $? != 0 ]]; then
     record_log "Partition might be corrupted"
@@ -75,14 +75,14 @@ mountfs() {
 prepare_local_partition() {
   record_log "Preparing local partition ..."
 
-  MOUNT_POINT=$(mount -v | grep "/dev/nvme0n1p4" | awk '{print $3}' ||:)
+  MOUNT_POINT=$(mount -v | grep "/dev/nvme1n1" | awk '{print $3}' ||:)
 
   if [[ x"${MOUNT_POINT}" == x"${MOUNT_DIR}" ]]; then
-    record_log "/dev/nvme0n1p4 is already mounted on ${MOUNT_POINT}"
+    record_log "/dev/nvme1n1 is already mounted on ${MOUNT_POINT}"
     return
   fi
 
-  if [ x$(sudo file -sL /dev/nvme0n1p4 | grep -o ext4) == x"" ]; then
+  if [ x$(sudo file -sL /dev/nvme1n1 | grep -o ext4) == x"" ]; then
     create_extfs
   fi
 
