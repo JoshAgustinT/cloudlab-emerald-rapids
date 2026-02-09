3603da8c440e9f7db3aed1502438dabe236588dc
JoshAgustinT <89602882+JoshAgustinT@users.noreply.github.com>
2026-02-09 02:27:13 -0700

    Toggle hardware type configurations in profile.py

--- profile.py
+++ profile.py
@@ -22,13 +22,13 @@ node_0 = request.RawPC('node-0')
 # node_0.hardware_type = 'r6615'
 
 # Intel Xeon 8360Y 
-# node_0.hardware_type = 'r650'
+node_0.hardware_type = 'r650'
 
 # Intel Xeon Gold 6142 Processor
 # node_0.hardware_type = 'c6420'
 
 # 128 thread dual socket: Intel Xeon Gold 6548Y+
-node_0.hardware_type = 'd760'
+# node_0.hardware_type = 'd760'
 # 1 socket emmerald machine 56 threads
 # node_0.hardware_type = 'c6620'
 
