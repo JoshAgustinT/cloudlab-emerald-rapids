895cf1d61ea9611b4aea914685df4860986d30ec
JoshAgustinT <89602882+JoshAgustinT@users.noreply.github.com>
2026-04-13 08:32:35 -0600

    Toggle hardware_type configuration for node_0

--- profile.py
+++ profile.py
@@ -22,7 +22,7 @@ node_0 = request.RawPC('node-0')
 
 # AMD EPYC 9354P zen4
 # node_0.hardware_type = 'r6615'
-node_0.hardware_type = 'd7615' # 1 numa node config
+# node_0.hardware_type = 'd7615' # 1 numa node config
 
 # Intel Xeon 8360Y ice lake
 # node_0.hardware_type = 'r650'
@@ -31,7 +31,7 @@ node_0.hardware_type = 'd7615' # 1 numa node config
 # node_0.hardware_type = 'c6420'
 
 # 128 thread dual socket: Intel Xeon Gold 6548Y+
-# node_0.hardware_type = 'd760'
+node_0.hardware_type = 'd760'
 # 1 socket emmerald machine 56 threads
 # node_0.hardware_type = 'c6620'
 
